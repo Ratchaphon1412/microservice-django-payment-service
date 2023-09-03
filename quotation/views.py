7489355d678ca1bd693c625b12387ee1f74cb3cd
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-09-03 22:44:43 +0700

    quotation app

---
+++ quotation/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import *
+from service.renderpdf import render_to_pdf
+import datetime
+# Create your views here.
+
+class QuotationAPIView(APIView):
+    def get(self,request):
+        data = {
+            'today': datetime.date.today(), 
+            'amount': 39.99,
+            'customer_name': 'Cooper Mann',
+            'invoice_number': 1233434,
+        }
+        pdf = render_to_pdf('ksn/quotation.html', data)
+        
+        return Response(pdf);
+    
+    def post(self,request):
+        return "hello post"
