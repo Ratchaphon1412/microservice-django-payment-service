0d3170bd6b7217e695c75021ca79eac3a3cc9471
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-09-09 16:46:28 +0700

    config in setting read data from .env

--- djangopayment/settings.py
+++ djangopayment/settings.py
@@ -129,13 +129,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = os.getenv('LANGUAGE_CODE')
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = os.getenv('TIME_ZONE')
 
-USE_I18N = True
+USE_I18N = os.getenv('USE_I18N')
 
-USE_TZ = True
+USE_TZ = os.getenv('USE_TZ')
 
 
 # Static files (CSS, JavaScript, Images)
