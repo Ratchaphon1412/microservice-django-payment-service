2dfc0b5fad1513aa199f0342bf802623a53e6a81
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-09-12 01:25:18 +0700

    create Command for Consumer in payment app

---
+++ payment/management/commands/PaymentConsumerCommand.py
@@ -0,0 +1,32 @@
+from django.core.management.base import BaseCommand, CommandError
+from Infastructure.kafka.consumer import Consumer
+
+class Command(BaseCommand):
+    help = 'Starts the consumer'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--topic', nargs='+', type=str,action='append' ,help='Topic name')
+        parser.add_argument('--group', nargs='+', type=str,help='Group name')
+
+    def handle(self, *args, **options):
+        try:
+            listTopic = []
+            groupConsumer = ""
+            
+            # # for topic in options['--topic']:
+            # #     self.stdout.write(self.style.SUCCESS(topic))
+            if options['topic']:
+                listTopic = options['topic'][0]
+            
+            
+            if options['group']:
+                groupConsumer = options['group'][0]
+                
+        
+            consumer = Consumer(listTopic,groupConsumer)
+            consumer.start()
+        except Exception as e:
+            
+            self.stderr.write(self.style.ERROR(e))
+            raise CommandError('Consumer failed to start')
+        self.stdout.write(self.style.SUCCESS('Consumer started successfully'))
