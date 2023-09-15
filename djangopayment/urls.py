79d6a47832e331da8cfaacd1d342fb53a841b7ea
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-09-15 20:54:28 +0700

    add route payment app

--- djangopayment/urls.py
+++ djangopayment/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('quotation.urls')),
+    path('api/v1/', include('payment.urls')),
 
 ]
 
