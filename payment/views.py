241f79396378c26ce6d9bb70bde94371fc589ead
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-09-15 20:54:55 +0700

    create view class for test payment connect omise

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from Infastructure.service import Facade
 # Create your views here.
+
+
+class PaymentApi(APIView):
+    def get(self,request):
+        return Response({'Balance':Facade.omiseService().adminBalance()})
+    
+class CreditCardApi(APIView):
+    def get(self,request):
+        pass
