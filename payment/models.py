b45b3b54c967070ae468e901bc3a72d4fd0730c7
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-11-04 00:34:43 +0700

    create user model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    id = models.CharField(max_length=255,primary_key=True)
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    customer_omise_id = models.CharField(max_length=255, blank=True, null=True)
