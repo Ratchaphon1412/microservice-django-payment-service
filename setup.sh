bb3d74c9251e3408b52ddca4ed6f60cd3dd385a7
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-11-06 08:23:11 +0700

    edit deamon mode

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 poetry run python3 manage.py collectstatic --noinput
 poetry run python3 manage.py makemigrations 
 poetry run python3 manage.py migrate
-poetry run gunicorn --bind 0.0.0.0:8023 djangopayment.wsgi
+poetry run gunicorn --bind 0.0.0.0:8023 djangopayment.wsgi --daemon
 poetry run poetry run python3 manage.py ConsumerCommand --topic create_user --group auth 
